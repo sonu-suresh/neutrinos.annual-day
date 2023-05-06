dcdacd2e9e5795ab3f9f5c8713cef3619d323eca
Yeshaswini-s <yeshaswini2433@gmail.com>
2023-05-06 19:15:42 +0530

    added form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,132 @@ import {FormlyFieldConfig} from '@ngx-formly/core';
 })
 export class AppComponent {
   form = new FormGroup({});
-  model = { email: 'email@gmail.com' };
+  model = {};
   fields: FormlyFieldConfig[] = [
     {
-      key: 'email',
+      key: 'select',
+      type: 'select',
+      props: {
+        label: 'Are you attending the event?',
+        placeholder: 'Select placeholder',
+        required: true,
+        options: [
+          { label: 'Yes', value: 'yes' },
+          { label: 'No', value: 'no' },
+        ],
+      },
+    },
+    {
+      key: 'radio',
+      type: 'select',
+      props: {
+        label: 'Would you be attending online or offline? ',
+        placeholder: 'Input placeholder',
+        required: true,
+        options: [
+          { label: 'Online', value: 'online' },
+          { label: 'Offline', value: 'offline' },
+        ]
+      },
+    },
+    {
+      key: 'name',
       type: 'input',
       props: {
-        label: 'Email address',
-        placeholder: 'Enter email',
+        label: 'Name ',
+        placeholder: 'Enter full name',
         required: true,
-      }
-    }
+       
+      },
+    },
+    {
+      key: 'employeeNumber',
+      type: 'input',
+      props: {
+        label: 'Employee Number',
+        placeholder: 'Enter employee number',
+        required: true,
+       
+      },
+    },
+    {
+      key: 'foodPreference',
+      type: 'select',
+      props: {
+        label: 'Food Preference',
+        placeholder: 'Select Food Preference',
+        required: true,
+        options: [
+          { label: 'Vegetarian', value: 'veg' },
+          { label: 'Non Vegetarian', value: 'non-veg' },
+        ],
+      },
+    },
+    {
+      key: 'shirtSize',
+      type: 'select',
+      props: {
+        label: 'T-shirt Size',
+        placeholder: 'Select T-shirt Size',
+        required: true,
+        options: [
+          { label: 'S- Small', value: 's-small' },
+          { label: 'M- Medium', value: 'm-medium' },
+          { label: 'L- Large', value: 'l-large' },
+          { label: 'XL- Large', value: 'xl-extralarge' },
+
+
+        ],
+      },
+    },
+    {
+      key: 'areaofDrop',
+      type: 'textarea',
+      props: {
+        label: 'Preffered area of drop',
+        placeholder: 'Preffered area of drop',
+        required: false,
+        
+      },
+    },
+    // {
+    //   key: 'checkbox',
+    //   type: 'checkbox',
+    //   props: {
+    //     label: 'Checkbox',
+    //   },
+    // },
+    // {
+    //   key: 'select',
+    //   type: 'select',
+    //   props: {
+    //     label: 'Select',
+    //     placeholder: 'Select placeholder',
+    //     required: true,
+    //     options: [
+    //       { label: 'Option 1', value: '1' },
+    //       { label: 'Option 2', value: '2' },
+    //       { label: 'Option 3', value: '3' },
+    //     ],
+    //   },
+    // },
+    // {
+    //   key: 'radio',
+    //   type: 'radio',
+    //   props: {
+    //     label: 'Radio',
+    //     required: true,
+    //     options: [
+    //       { label: 'Option 1', value: '1' },
+    //       { label: 'Option 2', value: '2' },
+    //     ],
+    //   },
+    // },
   ];
 
-  onSubmit(model:any) {
-    console.log(model);
+  onSubmit() {
+    if (this.form.valid) {
+      alert(JSON.stringify(this.model, null, 2));
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-   import { ReactiveFormsModule } from '@angular/forms';
-   import { FormlyModule } from '@ngx-formly/core';
-   import { FormlyMaterialModule } from '@ngx-formly/material';
-   import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  imports: [ 
     BrowserAnimationsModule,
-    BrowserModule,
-    AppRoutingModule,
-     ReactiveFormsModule,
-  FormlyModule.forRoot(),
-  FormlyMaterialModule
+    MatButtonModule,
+
+    ReactiveFormsModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+      ],
+    }),
+    FormlyMaterialModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: appearance
+    }
+  ],
 })
 export class AppModule { }
